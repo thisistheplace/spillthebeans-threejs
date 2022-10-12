d243fa6bf6d30b67970b4c71e2ef853f106d4b58
Ben Cannell <bencannell@Bens-MacBook-Air.local>
2022-10-12 16:31:25 +0100

    Rebuilt python, v0.0.5

--- spillthebeans_threejs/SpillthebeansThreejs.py
+++ spillthebeans_threejs/SpillthebeansThreejs.py
@@ -13,27 +13,27 @@ Keyword arguments:
     The ID used to identify the container for the IFC viewer
     component.
 
-- maxAngle (number; required)
+- canAngle (number; required)
 
-- numBeans (number; required)
+- rotation (number; default 0.01)
 
-- rotation (number; required)"""
+- scale (number; default 0.5)"""
     _children_props = []
     _base_nodes = ['children']
     _namespace = 'spillthebeans_threejs'
     _type = 'SpillthebeansThreejs'
     @_explicitize_args
-    def __init__(self, id=Component.REQUIRED, numBeans=Component.REQUIRED, rotation=Component.REQUIRED, maxAngle=Component.REQUIRED, axis=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'maxAngle', 'numBeans', 'rotation']
+    def __init__(self, id=Component.REQUIRED, canAngle=Component.REQUIRED, scale=Component.UNDEFINED, rotation=Component.UNDEFINED, axis=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'canAngle', 'rotation', 'scale']
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'maxAngle', 'numBeans', 'rotation']
+        self.available_properties = ['id', 'canAngle', 'rotation', 'scale']
         self.available_wildcard_properties =            []
         _explicit_args = kwargs.pop('_explicit_args')
         _locals = locals()
         _locals.update(kwargs)  # For wildcard attrs and excess named props
         args = {k: _locals[k] for k in _explicit_args}
 
-        for k in ['id', 'maxAngle', 'numBeans', 'rotation']:
+        for k in ['id', 'canAngle']:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
